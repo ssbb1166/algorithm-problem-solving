3d3b1954b8f800b9bc2d8fa49d40e737b5f29dfb
Subin Song <mugong66@naver.com>
2023-04-13 22:27:56 +0900

    백준 1753 주석 추가

--- boj1753.cpp
+++ boj1753.cpp
@@ -4,54 +4,58 @@
 using namespace std;
 
 
-int n, e, k, u, v, w, d[20001];
+// 정점의 개수(n), 간선의 개수(e), 시작 정점의 번호(k)
+// (u, v, w): u에서 v로 가는 가중치 w인 간선
+int n, e, k, u, v, w, dist[20001];
 vector<pair<int, int>> graph[20001];
 
 
 void Dijkstra(int start)
 {
-	priority_queue<pair<int, int>> pq;
-	pq.push({ 0, start });
-	d[start] = 0;
+    priority_queue<pair<int, int>> pq;
+    pq.push({ 0, start });
+    dist[start] = 0;
 
-	while (!pq.empty()) {
-		int dist = -pq.top().first;
-		int now = pq.top().second;
-		pq.pop();
+    while (!pq.empty()) {
+        int d = -pq.top().first;
+        int x = pq.top().second;
+        pq.pop();
 
-		if (d[now] < dist) continue;
+        if (d > dist[x]) continue;
 
-		for (int i = 0; i < graph[now].size(); i++) {
-			int next = graph[now][i].first;
-			int cost = dist + graph[now][i].second;
-			if (cost < d[next]) {
-				pq.push({ -cost, next });
-				d[next] = cost;
-			}
-		}
-	}
+        for (int i = 0; i < graph[x].size(); i++) {
+            int nx = graph[x][i].first;
+            int nd = d + graph[x][i].second;
+            // 우선순위 큐는 값이 클수록 우선순위가 높음
+            // 거리가 짧은 정점을 먼저 처리하기 위해 음수로 삽입
+            if (nd < dist[nx]) {
+                pq.push({ -nd, nx });
+                dist[nx] = nd;
+            }
+        }
+    }
 }
 
 
 int main()
 {
-	cin >> n >> e >> k;
+    cin >> n >> e >> k;
 
-	for (int i = 0; i < e; i++) {
-		cin >> u >> v >> w;
-		graph[u].push_back({ v, w });
-	}
+    for (int i = 0; i < e; i++) {
+        cin >> u >> v >> w;
+        graph[u].push_back({ v, w });
+    }
 
-	for (int i = 1; i <= n; i++)
-		d[i] = INF;
+    for (int i = 1; i <= n; i++)
+        dist[i] = INF;
 
-	Dijkstra(k);
+    Dijkstra(k);
 
-	for (int i = 1; i <= n; i++)
-		if (d[i] == INF)
-			cout << "INF" << '\n';
-		else
-			cout << d[i] << '\n';
+    for (int i = 1; i <= n; i++)
+        if (dist[i] == INF)
+            cout << "INF" << '\n';
+        else
+            cout << dist[i] << '\n';
 
-	return 0;
+    return 0;
 }
