ee509b3691d720eb38594dabe41699d4e2cf6ae7
Subin Song <mugong66@naver.com>
2023-04-16 10:48:04 +0900

    백준 1238 업로드

--- boj1238.cpp
+++ boj1238.cpp
@@ -10,54 +10,54 @@ vector<pair<int, int>> graph[1001][2];
 
 void Dijkstra(int dir)
 {
-	priority_queue<pair<int, int>> pq;
-	pq.push({ 0, x });
-	dist[x][dir] = 0;
+    priority_queue<pair<int, int>> pq;
+    pq.push({ 0, x });
+    dist[x][dir] = 0;
 
-	while (!pq.empty()) {
-		int d = -pq.top().first;
-		int x = pq.top().second;
-		pq.pop();
+    while (!pq.empty()) {
+        int d = -pq.top().first;
+        int x = pq.top().second;
+        pq.pop();
 
-		if (d < dist[x][dir]) continue;
+        if (d < dist[x][dir]) continue;
 
-		for (int i = 0; i < graph[x][dir].size(); i++) {
-			int nx = graph[x][dir][i].first;
-			int nd = d + graph[x][dir][i].second;
-			if (nd < dist[nx][dir]) {
-				pq.push({ -nd, nx });
-				dist[nx][dir] = nd;
-			}
-		}
-	}
+        for (int i = 0; i < graph[x][dir].size(); i++) {
+            int nx = graph[x][dir][i].first;
+            int nd = d + graph[x][dir][i].second;
+            if (nd < dist[nx][dir]) {
+                pq.push({ -nd, nx });
+                dist[nx][dir] = nd;
+            }
+        }
+    }
 }
 
 
 int main()
 {
-	cin >> n >> m >> x;
+    cin >> n >> m >> x;
 
-	for (int i = 0; i < m; i++) {
-		cin >> u >> v >> t;
-		graph[u][0].push_back({ v, t });
-		graph[v][1].push_back({ u, t });
-	}
-	
-	for (int i = 1; i <= n; i++) {
-		dist[i][0] = INF;
-		dist[i][1] = INF;
-	}
+    for (int i = 0; i < m; i++) {
+        cin >> u >> v >> t;
+        graph[u][0].push_back({ v, t });
+        graph[v][1].push_back({ u, t });
+    }
+    
+    for (int i = 1; i <= n; i++) {
+        dist[i][0] = INF;
+        dist[i][1] = INF;
+    }
 
-	Dijkstra(0);  // 가는 데 걸리는 최단 시간
-	Dijkstra(1);  // 오는 데 걸리는 최단 시간
+    Dijkstra(0);  // 가는 데 걸리는 최단 시간
+    Dijkstra(1);  // 오는 데 걸리는 최단 시간
 
-	for (int i = 1; i <= n; i++) {
-		int tmp = dist[i][0] + dist[i][1];
-		if (tmp > max_t)
-			max_t = tmp;
-	}
+    for (int i = 1; i <= n; i++) {
+        int tmp = dist[i][0] + dist[i][1];
+        if (tmp > max_t)
+            max_t = tmp;
+    }
 
-	cout << max_t << '\n';
+    cout << max_t << '\n';
 
-	return 0;
+    return 0;
 }
