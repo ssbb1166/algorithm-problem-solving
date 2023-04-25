0e6b23c5e454f9372433077439c71e351757749a
Subin Song <mugong66@naver.com>
2023-04-25 22:49:35 +0900

    백준 4195 업로드

---
+++ boj4195.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+using namespace std;
+
+
+int t, f;
+string a, b;
+map<string, int> cnt;
+map<string, string> parent;
+
+
+string Find(string x)
+{
+    if (x == parent[x]) return x;
+    return parent[x] = Find(parent[x]);
+}
+
+
+void Union(string a, string b)
+{
+    a = Find(a);
+    b = Find(b);
+    if (a != b) {
+        parent[b] = a;
+        cnt[a] += cnt[b];
+    }
+}
+
+
+int main()
+{
+    // cin 쓰면 속도가 느려서 추가한 코드
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> t;
+
+    for (int i = 0; i < t; i++) {
+        cnt.clear();
+        parent.clear();
+
+        cin >> f;
+
+        for (int j = 0; j < f; j++) {
+            cin >> a >> b;
+
+            if (!parent.count(a)) {
+                parent[a] = a;
+                cnt[a] = 1;
+            }
+
+            if (!parent.count(b)) {
+                parent[b] = b;
+                cnt[b] = 1;
+            }
+
+            Union(a, b);
+
+            cout << cnt[Find(a)] << '\n';
+        }
+    }
+
+    return 0;
+}
