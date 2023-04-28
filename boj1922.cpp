72fa039a53f411b6fc9f0c7a29acee9e25e6d50a
Subin Song <mugong66@naver.com>
2023-04-28 15:20:46 +0900

    백준 1922 업로드

---
+++ boj1922.cpp
@@ -0,0 +1,55 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int n, m, a, b, c, res, parent[1001];
+vector<pair<int, pair<int, int>>> edge;
+
+
+int Find(int x)
+{
+    if (x == parent[x]) return x;
+    return parent[x] = Find(parent[x]);
+}
+
+
+void Union(int a, int b)
+{
+    a = Find(a);
+    b = Find(b);
+    if (a < b) parent[b] = a;
+    else parent[a] = b;
+}
+
+
+int main()
+{
+    cin >> n >> m;
+
+    for (int i = 0; i < m; i++) {
+        cin >> a >> b >> c;
+        edge.push_back({ c, {a, b} });
+    }
+
+    sort(edge.begin(), edge.end());
+
+    for (int i = 1; i <= n; i++)
+        parent[i] = i;
+
+    for (int i = 0; i < edge.size(); i++) {
+        a = edge[i].second.first;
+        b = edge[i].second.second;
+        c = edge[i].first;
+
+        if (Find(a) != Find(b)) {
+            Union(a, b);
+            res += c;
+        }
+    }
+
+    cout << res << '\n';
+
+    return 0;
+}
