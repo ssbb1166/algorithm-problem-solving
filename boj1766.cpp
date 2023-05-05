c278058a056fe29919f68139287593159ac5fa57
Subin Song <mugong66@naver.com>
2023-05-05 21:33:50 +0900

    백준 1766 업로드

---
+++ boj1766.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+
+int n, m, a, b, indegree[32001];
+vector<int> graph[32001];
+
+
+void TopologySort()
+{
+	priority_queue<int> q;
+
+	for (int i = 1; i <= n; i++)
+		if (!indegree[i]) q.push(-i);
+
+	while (!q.empty()) {
+		int x = -q.top();
+		q.pop();
+
+		cout << x << ' ';
+
+		for (int i = 0; i < graph[x].size(); i++) {
+			int nx = graph[x][i];
+			indegree[nx]--;
+			if (!indegree[nx]) q.push(-nx);
+		}
+	}
+}
+
+
+int main()
+{
+	cin >> n >> m;
+
+	for (int i = 0; i < m; i++) {
+		cin >> a >> b;
+		graph[a].push_back(b);
+		indegree[b]++;
+	}
+
+	TopologySort();
+
+	return 0;
+}
