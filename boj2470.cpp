4f191a2251e098801b4610f43722fc05f28cff32
Subin Song <mugong66@naver.com>
2023-05-12 20:08:57 +0900

    백준 2470 업로드

---
+++ boj2470.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+
+int n, sum, min_sum = 2e9, value[100000], res[2];
+
+
+int main()
+{
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+        cin >> value[i];
+
+    sort(value, value + n);
+
+    int start = 0, end = n - 1;
+    while (start < end) {
+        sum = value[start] + value[end];
+        if (abs(sum) < min_sum) {
+            min_sum = abs(sum);
+            res[0] = value[start];
+            res[1] = value[end];
+        }
+        if (sum < 0) start++;
+        else end--;
+    }
+
+    cout << res[0] << ' ' << res[1] << '\n';
+
+    return 0;
+}
