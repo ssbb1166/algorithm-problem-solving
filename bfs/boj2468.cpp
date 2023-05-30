77b123af13d38e63ddd54a57b8e297af0b4ff906
Subin Song <mugong66@naver.com>
2023-05-30 18:40:02 +0900

    feat: add bfs/boj2468.cpp

---
+++ bfs/boj2468.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+
+int n, max_h, cnt, max_cnt;
+int map[100][100], tmp[100][100];
+
+
+int dx[] = { 0, 0, -1, 1 };
+int dy[] = { -1, 1, 0, 0 };
+
+
+void BFS(int x, int y)
+{
+    queue<pair<int, int>> q;
+    q.push({ x, y });
+    tmp[y][x] = 0;
+
+    while (!q.empty()) {
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || nx >= n || ny < 0 || ny >= n)
+                continue;
+
+            if (tmp[ny][nx] == 1) {
+                q.push({ nx, ny });
+                tmp[ny][nx] = 0;
+            }
+        }
+    }
+}
+
+
+int main()
+{
+    cin >> n;
+
+    for (int y = 0; y < n; y++) {
+        for (int x = 0; x < n; x++) {
+            cin >> map[y][x];
+            if (map[y][x] > max_h)
+                max_h = map[y][x];
+        }
+    }
+
+    for (int h = 0; h <= max_h; h++) {
+        for (int y = 0; y < n; y++)
+            for (int x = 0; x < n; x++)
+                if (map[y][x] > h)
+                    tmp[y][x] = 1;
+                else
+                    tmp[y][x] = 0;
+
+        cnt = 0;
+        for (int y = 0; y < n; y++) {
+            for (int x = 0; x < n; x++) {
+                if (tmp[y][x]) {
+                    BFS(x, y);
+                    cnt++;
+                }
+            }
+        }
+
+        if (cnt > max_cnt)
+            max_cnt = cnt;
+    }
+
+    cout << max_cnt << '\n';
+
+    return 0;
+}
