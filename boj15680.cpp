a22a5f0cb9b677882cfdbd0779f110abc0a41a1b
Subin Song <mugong66@naver.com>
2023-09-13 11:20:56 +0900

    feat: add boj15680.cpp

---
+++ boj15680.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int n;
+
+
+int main()
+{
+    cin >> n;
+
+    if (n == 0)
+        cout << "YONSEI" << '\n';
+    else
+        cout << "Leading the Way to the Future" << '\n';
+
+    return 0;
+}
