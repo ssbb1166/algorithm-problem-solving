3e051b41601ac7daa77264d8f9009a700d896e58
Subin Song <mugong66@naver.com>
2023-10-19 15:56:43 +0900

    feat: add boj8370.cpp

---
+++ boj8370.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int n1, k1, n2, k2;
+
+
+int main()
+{
+    cin >> n1 >> k1 >> n2 >> k2;
+
+    cout << n1 * k1 + n2 * k2 << '\n';
+
+    return 0;
+}
