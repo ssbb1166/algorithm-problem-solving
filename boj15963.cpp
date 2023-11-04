9d77c1a7c517148eca3d735c45942c9049bdb8ee
Subin Song <mugong66@naver.com>
2023-11-04 20:53:29 +0900

    feat: add boj15963.cpp

---
+++ boj15963.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+long long int n, m;
+
+
+int main()
+{
+    cin >> n >> m;
+
+    if (n == m)
+        cout << 1 << '\n';
+    else
+        cout << 0 << '\n';
+
+    return 0;
+}
