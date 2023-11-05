889899dc92fbd0e73bd380eb7fb90d18f387777c
Subin Song <mugong66@naver.com>
2023-11-05 13:22:40 +0900

    feat: add boj14581.cpp

---
+++ boj14581.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+string id;
+
+
+int main()
+{
+    cin >> id;
+
+    cout << ":fan::fan::fan:\n";
+    cout << ":fan::" << id << "::fan:\n";
+    cout << ":fan::fan::fan:\n";
+
+    return 0;
+}
