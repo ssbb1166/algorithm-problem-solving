0f98bef0c1d06b8f98690f16572d1f57d87e1108
Subin Song <mugong66@naver.com>
2023-11-06 10:49:11 +0900

    feat: add boj20254.cpp

---
+++ boj20254.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int ur, tr, uo, to;
+
+
+int main()
+{
+    cin >> ur >> tr >> uo >> to;
+
+    cout << 56 * ur + 24 * tr + 14 * uo + 6 * to << '\n';
+
+    return 0;
+}
