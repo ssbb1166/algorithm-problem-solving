9eff215f4d92b18b261ff44433d5b89bde1b4578
Subin Song <mugong66@naver.com>
2023-12-15 19:57:15 +0900

    feat: add boj1267.cpp

---
+++ boj1267.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+
+int n, t, y, m;
+
+
+int main()
+{
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> t;
+        y += (t / 30 + 1) * 10;
+        m += (t / 60 + 1) * 15;
+    }
+
+    if (y < m)
+        cout << "Y " << y << '\n';
+    else if (y > m)
+        cout << "M " << m << '\n';
+    else
+        cout << "Y M " << y << '\n';
+
+    return 0;
+}
