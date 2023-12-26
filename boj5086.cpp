fbb8386ee1f47c36f3ead0afc20ee3636e16fa4e
Subin Song <mugong66@naver.com>
2023-12-27 02:48:57 +0900

    feat: add boj5086.cpp

---
+++ boj5086.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+int a, b;
+
+
+int main()
+{
+    while (1) {
+        cin >> a >> b;
+
+        if (a == 0 && b == 0) break;
+
+        if (a < b && b % a == 0)
+            cout << "factor" << '\n';
+        else if (a > b && a % b == 0)
+            cout << "multiple" << '\n';
+        else
+            cout << "neither" << '\n';
+    }
+
+    return 0;
+}
