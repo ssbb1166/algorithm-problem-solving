f2c7c0440db8066cb82def09a6df4f820c1b2b05
Subin Song <mugong66@naver.com>
2023-12-29 14:39:25 +0900

    feat: add boj2445.cpp

---
+++ boj2445.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+
+int n;
+
+
+int main()
+{
+    cin >> n;
+
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < i; j++)
+            cout << '*';
+        for (int j = 0; j < n - i; j++)
+            cout << ' ';
+        for (int j = 0; j < n - i; j++)
+            cout << ' ';
+        for (int j = 0; j < i; j++)
+            cout << '*';
+        cout << '\n';
+    }
+
+    for (int i = n; i > 0; i--) {
+        for (int j = 0; j < i; j++)
+            cout << '*';
+        for (int j = 0; j < n - i; j++)
+            cout << ' ';
+        for (int j = 0; j < n - i; j++)
+            cout << ' ';
+        for (int j = 0; j < i; j++)
+            cout << '*';
+        cout << '\n';
+    }
+
+    return 0;
+}
