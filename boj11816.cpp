94d665b61a7b441fed7749c8387ac3303bb96ac3
Subin Song <mugong66@naver.com>
2024-04-02 19:40:45 +0900

    feat: add boj11816.cpp

---
+++ boj11816.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+string x;
+
+
+int main()
+{
+	cin >> x;
+
+	if (x[0] == '0' && x[1] == 'x')
+		cout << stoi(x, nullptr, 16) << '\n';
+	else if (x[0] == '0')
+		cout << stoi(x, nullptr, 8) << '\n';
+	else
+		cout << x << '\n';
+
+	return 0;
+}
