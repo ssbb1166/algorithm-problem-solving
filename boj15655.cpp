54ba82006bd2a922277bbb2587153a36f5206bc0
Subin Song <mugong66@naver.com>
2024-04-17 21:54:31 +0900

    feat: add boj15655.cpp

---
+++ boj15655.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+
+int n, m, num[8], seq[8];
+
+
+void FindSequence(int cnt, int idx)
+{
+    if (cnt == m) {
+        for (int i = 0; i < m; i++)
+            cout << seq[i] << ' ';
+        cout << '\n';
+        return;
+    }
+
+    for (int i = idx; i < n; i++) {
+        seq[cnt] = num[i];
+        FindSequence(cnt + 1, i + 1);
+    }
+}
+
+
+int main()
+{
+    cin >> n >> m;
+    
+    for (int i = 0; i < n; i++)
+        cin >> num[i];
+
+    sort(num, num + n);
+
+    FindSequence(0, 0);
+
+    return 0;
+}
