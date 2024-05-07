4f75a96f00b0e9d90a6179a38f1d49d0fb10ad0f
Subin Song <mugong66@naver.com>
2024-05-07 20:19:14 +0900

    fix: modify dp[i] calculation code in boj2156.cpp
    dp[i]가 i번째 포도주를 무조건 포함해서 선택하는 최대 포도주 양이라고 생각해서 틀렸다. 'i번째 포도주를 무조건 포함해서 선택한다'는 조건이 있어야 dp[i] 값을 계산할 수 있다고 생각했기 때문이다. dp[i] = max(dp[i], dp[i - 1]) 코드를 추가하면 dp[i]에 i개의 포도주 잔이 있을 때 최대로 마실 수 있는 포도주의 양을 저장할 수 있다.

--- dp/boj2156.cpp
+++ dp/boj2156.cpp
@@ -16,10 +16,12 @@ int main()
     dp[1] = wine[1];
     dp[2] = wine[1] + wine[2];
 
-    for (int i = 3; i <= n; i++)
+    for (int i = 3; i <= n; i++) {
         dp[i] = max(dp[i - 2] + wine[i], dp[i - 3] + wine[i - 1] + wine[i]);
+        dp[i] = max(dp[i], dp[i - 1]);
+    }
 
-    cout << max(dp[n], dp[n - 1]) << '\n';
+    cout << dp[n] << '\n';
 
 
     return 0;
