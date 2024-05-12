1e72cb961e566e7297314636c4a9580dc8119588
Subin Song <mugong66@naver.com>
2024-05-12 09:59:31 +0900

    style: edit indentation in boj2295.cpp

--- binary-search/boj2295.cpp
+++ binary-search/boj2295.cpp
@@ -10,25 +10,25 @@ vector<int> two;
 
 int main()
 {
-	cin >> n;
+    cin >> n;
 
-	for (int i = 1; i <= n; i++)
-		cin >> u[i];
+    for (int i = 1; i <= n; i++)
+        cin >> u[i];
 
-	sort(u + 1, u + n + 1);
+    sort(u + 1, u + n + 1);
 
-	for (int x = 1; x <= n; x++)
-		for (int y = 1; y <= n; y++)
-			two.push_back(u[x] + u[y]);
+    for (int x = 1; x <= n; x++)
+        for (int y = 1; y <= n; y++)
+            two.push_back(u[x] + u[y]);
 
-	sort(two.begin(), two.end());
+    sort(two.begin(), two.end());
 
-	for (int k = n; k > 0; k--) {
-		for (int z = 1; z < k; z++) {
-			if (binary_search(two.begin(), two.end(), u[k] - u[z])) {
-				cout << u[k];
-				return 0;
-			}
-		}
-	}
+    for (int k = n; k > 0; k--) {
+        for (int z = 1; z < k; z++) {
+            if (binary_search(two.begin(), two.end(), u[k] - u[z])) {
+                cout << u[k];
+                return 0;
+            }
+        }
+    }
 }
