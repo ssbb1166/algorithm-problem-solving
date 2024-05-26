2fb05a4844655b4f36e3acac34823559bd3bb565
Subin Song <mugong66@naver.com>
2024-05-26 19:29:22 +0900

    refactor: variable name d to dist in boj1004.cpp

--- mathematics/boj1004.cpp
+++ mathematics/boj1004.cpp
@@ -10,14 +10,14 @@ int main()
     cin >> t;
 
     while (t--) {
-        int x1, y1, x2, y2, n, cx, cy, r, d1, d2, cnt = 0;
+        int x1, y1, x2, y2, n, cx, cy, r, dist1, dist2, cnt = 0;
         cin >> x1 >> y1 >> x2 >> y2 >> n;
 
         while (n--) {
             cin >> cx >> cy >> r;
-            d1 = (cx - x1) * (cx - x1) + (cy - y1) * (cy - y1);
-            d2 = (cx - x2) * (cx - x2) + (cy - y2) * (cy - y2);
-            cnt += (d1 < r* r) ^ (d2 < r* r);
+            dist1 = (cx - x1) * (cx - x1) + (cy - y1) * (cy - y1);
+            dist2 = (cx - x2) * (cx - x2) + (cy - y2) * (cy - y2);
+            cnt += (dist1 < r * r) ^ (dist2 < r * r);
         }
 
         cout << cnt << '\n';
