75659bf86f98e2fac96b9e1fb37681ed44e0172f
Subin Song <mugong66@naver.com>
2024-05-27 00:33:32 +0900

    feat: add implementation/boj1008.cpp

---
+++ implementation/boj1008.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+double a, b;
+
+
+int main()
+{
+    cin >> a >> b;
+    cout << fixed;
+    cout.precision(9);
+    cout << a / b;
+
+    return 0;
+}
