4b68c0513f9b6cc93bdaa46f57ffd06c46ba390e
Subin Song <mugong66@naver.com>
2024-05-26 19:18:23 +0900

    refactor: for loop to while loop in boj1004.cpp

--- mathematics/boj1004.cpp
+++ mathematics/boj1004.cpp
@@ -2,26 +2,22 @@
 using namespace std;
 
 
-int t, cnt;
+int t;
 
 
 int main()
 {
     cin >> t;
 
-    for (int i = 0; i < t; i++) {
-        int x1, y1, x2, y2, n, cx, cy, r, d1, d2;
-
+    while (t--) {
+        int x1, y1, x2, y2, n, cx, cy, r, d1, d2, cnt = 0;
         cin >> x1 >> y1 >> x2 >> y2 >> n;
 
-        cnt = 0;
-        for (int j = 0; j < n; j++) {
+        while (n--) {
             cin >> cx >> cy >> r;
-
             d1 = (cx - x1) * (cx - x1) + (cy - y1) * (cy - y1);
             d2 = (cx - x2) * (cx - x2) + (cy - y2) * (cy - y2);
-
-            if ((d1 < r * r) ^ (d2 < r * r)) cnt++;
+            cnt += (d1 < r* r) ^ (d2 < r* r);
         }
 
         cout << cnt << '\n';
