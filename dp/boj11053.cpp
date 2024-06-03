8e8823b424c2d0ccbb0ef2154bd6890cf1ed274a
Subin Song <mugong66@naver.com>
2024-06-03 17:32:47 +0900

    refactor: modify res calculation in boj11053.cpp

--- dp/boj11053.cpp
+++ dp/boj11053.cpp
@@ -13,19 +13,17 @@ int main()
     for (int i = 0; i < n; i++)
         cin >> a[i];
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++) {
         dp[i] = 1;
 
-    for (int i = 1; i < n; i++)
         for (int j = 0; j < i; j++)
             if (a[i] > a[j])
                 dp[i] = max(dp[i], dp[j] + 1);
 
-    for (int i = 0; i < n; i++)
-        if (dp[i] > res)
-            res = dp[i];
+        res = max(res, dp[i]);
+    }
 
-    cout << res << '\n';
+    cout << res;
 
     return 0;
 }
