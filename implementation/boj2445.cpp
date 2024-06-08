9103955dc8b4b9bea3f26e55d9a71d31cac19e8a
Subin Song <mugong66@naver.com>
2024-06-09 00:10:52 +0900

    refactor: modify for loop condition in boj2445.cpp

--- implementation/boj2445.cpp
+++ implementation/boj2445.cpp
@@ -9,26 +9,22 @@ int main()
 {
     cin >> n;
 
-    for (int i = 1; i < n; i++) {
-        for (int j = 0; j < i; j++)
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++)
             cout << '*';
-        for (int j = 0; j < n - i; j++)
+        for (int j = 1; j <= (n - i) * 2; j++)
             cout << ' ';
-        for (int j = 0; j < n - i; j++)
-            cout << ' ';
-        for (int j = 0; j < i; j++)
+        for (int j = 1; j <= i; j++)
             cout << '*';
         cout << '\n';
     }
 
-    for (int i = n; i > 0; i--) {
-        for (int j = 0; j < i; j++)
+    for (int i = n - 1; i >= 1; i--) {
+        for (int j = 1; j <= i; j++)
             cout << '*';
-        for (int j = 0; j < n - i; j++)
+        for (int j = 1; j <= (n - i) * 2; j++)
             cout << ' ';
-        for (int j = 0; j < n - i; j++)
-            cout << ' ';
-        for (int j = 0; j < i; j++)
+        for (int j = 1; j <= i; j++)
             cout << '*';
         cout << '\n';
     }
