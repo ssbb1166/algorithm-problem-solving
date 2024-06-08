1b730e767dfd37dbe68142501e0a9f00e8b85465
Subin Song <mugong66@naver.com>
2024-06-09 00:10:13 +0900

    refactor: modify for loop condition in boj2444.cpp

--- implementation/boj2444.cpp
+++ implementation/boj2444.cpp
@@ -10,17 +10,17 @@ int main()
     cin >> n;
 
     for (int i = 1; i <= n; i++) {
-        for (int j = 0; j < n - i; j++)
+        for (int j = 1; j <= n - i; j++)
             cout << ' ';
-        for (int j = 0; j < i * 2 - 1; j++)
+        for (int j = 1; j <= i * 2 - 1; j++)
             cout << '*';
         cout << '\n';
     }
 
-    for (int i = 1; i <= n - 1; i++) {
-        for (int j = 0; j < i; j++)
+    for (int i = n - 1; i >= 1; i--) {
+        for (int j = 1; j <= n - i; j++)
             cout << ' ';
-        for (int j = 0; j < (n - i) * 2 - 1; j++)
+        for (int j = 1; j <= 2 * i - 1; j++)
             cout << '*';
         cout << '\n';
     }
