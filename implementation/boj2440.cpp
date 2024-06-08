302e2b950402faf1f2f67c61228379dd8bd7e481
Subin Song <mugong66@naver.com>
2024-06-09 00:07:16 +0900

    feat: add implementation/boj2440.cpp

---
+++ implementation/boj2440.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int n;
+
+
+int main()
+{
+    cin >> n;
+
+    for (int i = n; i >= 1; i--) {
+        for (int j = 1; j <= i; j++)
+            cout << '*';
+        cout << '\n';
+    }
+
+    return 0;
+}
