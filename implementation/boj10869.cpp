44eaa5d6b6d95335e9adb8f80bb751203effa987
Subin Song <mugong66@naver.com>
2024-06-08 21:00:48 +0900

    feat: add implementation/boj10869.cpp

---
+++ implementation/boj10869.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int a, b;
+
+
+int main()
+{
+    cin >> a >> b;
+
+    cout << a + b << '\n';
+    cout << a - b << '\n';
+    cout << a * b << '\n';
+    cout << a / b << '\n';
+    cout << a % b;
+
+    return 0;
+}
