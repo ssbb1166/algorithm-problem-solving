e453a557771c0cd6cd5af54fac51e086d7c4cea4
Subin Song <mugong66@naver.com>
2024-06-14 07:05:08 +0900

    refactor: immediately output as input in boj20913.cpp

--- sorting/boj20913.cpp
+++ sorting/boj20913.cpp
@@ -3,27 +3,17 @@
 using namespace std;
 
 
-int n, p[100];
-string larry[100];
+int n, p;
 
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
     cin >> n;
 
     for (int i = 0; i < n; i++) {
-        cin >> p[i];
-        larry[i] += 1 + '0';
-        larry[i] += i / 10 + '0';
-        larry[i] += i % 10 + '0';
-    }
-
-    for (int i = 0; i < n; i++) {
-        cout << larry[i];
-        for (int j = 0; j < p[i]; j++)
+        cin >> p;
+        cout << 100 + i;
+        for (int j = 0; j < p; j++)
             cout << 0;
         cout << ' ';
     }
