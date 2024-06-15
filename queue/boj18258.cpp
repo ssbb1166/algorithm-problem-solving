990faa03f021c3a26cac3880679b4297c159c67e
Subin Song <mugong66@naver.com>
2024-06-16 04:41:08 +0900

    feat: add queue/boj18258.cpp

---
+++ queue/boj18258.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <queue>
+#include <string>
+using namespace std;
+
+
+int n, x;
+string cmd;
+queue<int> q;
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n;
+
+    while (n--) {
+        cin >> cmd;
+
+        if (cmd == "push") {
+            cin >> x;
+            q.push(x);
+        }
+        if (cmd == "pop") {
+            cout << (!q.empty() ? q.front() : -1) << '\n';
+            if (!q.empty()) q.pop();
+        }
+        if (cmd == "size")
+            cout << q.size() << '\n';
+        if (cmd == "empty")
+            cout << q.empty() << '\n';
+        if (cmd == "front")
+            cout << (!q.empty() ? q.front() : -1) << '\n';
+        if (cmd == "back")
+            cout << (!q.empty() ? q.back() : -1) << '\n';
+    }
+
+    return 0;
+}
