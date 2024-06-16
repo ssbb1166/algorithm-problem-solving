88e1999d23683ac1948002b6d6f1070ca0d4b21e
Subin Song <mugong66@naver.com>
2024-06-16 22:51:12 +0900

    style: remove vertical space in boj10866.cpp

--- deque/boj10866.cpp
+++ deque/boj10866.cpp
@@ -1,5 +1,6 @@
 #include <deque>
 #include <iostream>
+#include <string>
 using namespace std;
 
 
@@ -12,48 +13,31 @@ int main()
 {
     cin >> n;
 
-    for (int i = 0; i < n; i++) {
+    while (n--) {
         cin >> cmd;
 
         if (cmd == "push_front") {
             cin >> x;
             dq.push_front(x);
         }
-
         if (cmd == "push_back") {
             cin >> x;
             dq.push_back(x);
         }
-
         if (cmd == "pop_front") {
-            if (!dq.empty()) {
-                cout << dq.front() << '\n';
-                dq.pop_front();
-            }
-            else {
-                cout << -1 << '\n';
-            }
+            cout << (!dq.empty() ? dq.front() : -1) << '\n';
+            if (!dq.empty()) dq.pop_front();
         }
-
         if (cmd == "pop_back") {
-            if (!dq.empty()) {
-                cout << dq.back() << '\n';
-                dq.pop_back();
-            }
-            else {
-                cout << -1 << '\n';
-            }
+            cout << (!dq.empty() ? dq.back() : -1) << '\n';
+            if (!dq.empty()) dq.pop_back();
         }
-
         if (cmd == "size")
             cout << dq.size() << '\n';
-
         if (cmd == "empty")
-            cout << (dq.empty() ? 1 : 0) << '\n';
-
+            cout << dq.empty() << '\n';
         if (cmd == "front")
             cout << (!dq.empty() ? dq.front() : -1) << '\n';
-
         if (cmd == "back")
             cout << (!dq.empty() ? dq.back() : -1) << '\n';
     }
