0043be1d7885c9e7425fcc122148320feadf2d5a
Subin Song <mugong66@naver.com>
2024-06-17 16:35:02 +0900

    refactor: create function and change logic in boj9012.cpp
    - create VPS() function
    - change VPS check logic
    - replace for loop with range-based for loop

--- stack/boj9012.cpp
+++ stack/boj9012.cpp
@@ -5,37 +5,35 @@ using namespace std;
 
 
 int t;
-string s;
+string str;
+
+
+bool VPS()
+{
+    stack<char> st;
+    for (char c : str) {
+        if (c == '(')
+            st.push(c);
+        else if (!st.empty() && st.top() == '(')
+            st.pop();
+        else
+            return false;
+    }
+
+    if (!st.empty())
+        return false;
+    
+    return true;
+}
 
 
 int main()
 {
     cin >> t;
 
-    for (int i = 0; i < t; i++) {
-        stack<int> st;
-        bool vps = true;
-
-        cin >> s;
-
-        for (int j = 0; j < s.size(); j++) {
-            if (s[j] == '(')
-                st.push('(');
-            if (s[j] == ')') {
-                if (st.empty())
-                    vps = false;
-                else
-                    st.pop();
-            }
-        }
-
-        if (!st.empty())
-            vps = false;
-
-        if (vps)
-            cout << "YES" << '\n';
-        else
-            cout << "NO" << '\n';
+    while (t--) {
+        cin >> str;
+        cout << (VPS() ? "YES" : "NO") << '\n';
     }
 
     return 0;
