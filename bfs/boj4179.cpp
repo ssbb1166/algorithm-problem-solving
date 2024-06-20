14891fe210080cb179bd0610ab833bccb6d16d13
Subin Song <mugong66@naver.com>
2024-06-21 06:12:22 +0900

    feat: add bfs/boj4179.cpp

--- bfs/boj4179.cpp
+++ bfs/boj4179.cpp
@@ -1,87 +1,83 @@
-#include <algorithm>
 #include <iostream>
 #include <queue>
 #include <string>
-#include <vector>
 #define INF 1000000
 using namespace std;
 
 
-int r, c, map[1000][1000];
-queue<pair<int, int>> j, f;
-vector<int> times;
+int r, c, f[1000][1000], j[1000][1000];
+string map[1000];
+queue<pair<int, int>> fq, jq;
 
 
 int dx[] = { 0, 0, -1, 1 };
 int dy[] = { -1, 1, 0, 0 };
 
 
+void SpreadFire()
+{
+    while (!fq.empty()) {
+        int x = fq.front().first;
+        int y = fq.front().second;
+        fq.pop();
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || nx > c - 1 || ny < 0 || ny > r - 1)
+                continue;
+
+            if (map[ny][nx] != '#' && f[ny][nx] == INF) {
+                f[ny][nx] = f[y][x] + 1;
+                fq.push({ nx, ny });
+            }
+        }
+    }
+}
+
+
+void EscapeMaze()
+{
+    while (!jq.empty()) {
+        int x = jq.front().first;
+        int y = jq.front().second;
+        jq.pop();
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || nx > c - 1 || ny < 0 || ny > r - 1)
+                continue;
+
+            if (map[ny][nx] != '#' && j[ny][nx] == INF && j[y][x] + 1 < f[ny][nx]) {
+                j[ny][nx] = j[y][x] + 1;
+                jq.push({ nx, ny });
+            }
+        }
+    }
+}
+
+
 bool CheckEdge(int x, int y)
 {
-    if (x == 0 || x == c - 1 || y == 0 || y == r - 1)
-        return true;
-    return false;
+    return x == 0 || x == c - 1 || y == 0 || y == r - 1;
 }
 
 
-void Escape()
+int GetMinTime()
 {
-    int j_size = j.size();
-    while (j_size--) {
-        int x = j.front().first;
-        int y = j.front().second;
-        j.pop();
+    SpreadFire();
+    EscapeMaze();
 
-        if (map[y][x] > 0 && CheckEdge(x, y))
-            times.push_back(map[y][x]);
+    int res = INF;
+    for (int y = 0; y < r; y++)
+        for (int x = 0; x < c; x++)
+            if (CheckEdge(x, y))
+                res = min(res, j[y][x]);
 
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (nx < 0 || nx >= c || ny < 0 || ny >= r)
-                continue;
-
-            if (map[ny][nx] == 0) {
-                j.push({ nx, ny });
-                map[ny][nx] = map[y][x] + 1;
-            }
-        }
-    }
-
-    int f_size = f.size();
-    while (f_size--) {
-        int x = f.front().first;
-        int y = f.front().second;
-        f.pop();
-
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (nx < 0 || nx >= c || ny < 0 || ny >= r)
-                continue;
-
-            if (map[ny][nx] != -1) {
-                f.push({ nx, ny });
-                map[ny][nx] = -1;
-            }
-        }
-    }
-}
-
-
-int GetMinimumEscapeTime()
-{
-    while (!j.empty())
-        Escape();
-
-    sort(times.begin(), times.end());
-
-    if (times.empty())
-        return INF;
-
-    return times[0];
+    return res;
 }
 
 
@@ -90,26 +86,22 @@ int main()
     cin >> r >> c;
 
     for (int y = 0; y < r; y++) {
-        string line;
-        cin >> line;
+        cin >> map[y];
         for (int x = 0; x < c; x++) {
-            if (line[x] == '#')
-                map[y][x] = -1;
-            if (line[x] == '.')
-                map[y][x] = 0;
-            if (line[x] == 'J') {
-                map[y][x] = 1;
-                j.push({ x, y });
+            f[y][x] = j[y][x] = INF;
+            if (map[y][x] == 'F') {
+                f[y][x] = 1;
+                fq.push({ x, y });
             }
-            if (line[x] == 'F') {
-                map[y][x] = -1;
-                f.push({ x, y });
+            if (map[y][x] == 'J') {
+                j[y][x] = 1;
+                jq.push({ x, y });
             }
         }
     }
 
-    if (GetMinimumEscapeTime() != INF)
-        cout << GetMinimumEscapeTime();
+    if (GetMinTime() != INF)
+        cout << GetMinTime();
     else
         cout << "IMPOSSIBLE";
 
