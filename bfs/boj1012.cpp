7e2f48623de9066f3215bb0990cf2f049386830a
Subin Song <mugong66@naver.com>
2024-06-23 00:40:44 +0900

    refactor: add initialization function to boj1012.cpp

--- bfs/boj1012.cpp
+++ bfs/boj1012.cpp
@@ -3,15 +3,22 @@
 using namespace std;
 
 
-int t, m, n, k, x, y, cnt;
-int map[50][50];
+int t, m, n, k, x, y, cnt, map[50][50];
 
 
 int dx[] = { 0, 0, -1, 1 };
 int dy[] = { -1, 1, 0, 0 };
 
 
-void BFS(int x, int y)
+void Initialize()
+{
+    for (int y = 0; y < n; y++)
+        for (int x = 0; x < m; x++)
+            map[y][x] = 0;
+}
+
+
+void Count(int x, int y)
 {
     queue<pair<int, int>> q;
     q.push({ x, y });
@@ -26,7 +33,7 @@ void BFS(int x, int y)
             int nx = x + dx[i];
             int ny = y + dy[i];
 
-            if (nx < 0 || nx >= m || ny < 0 || ny >= n)
+            if (nx < 0 || nx > m - 1 || ny < 0 || ny > n - 1)
                 continue;
 
             if (map[ny][nx] == 1) {
@@ -35,6 +42,8 @@ void BFS(int x, int y)
             }
         }
     }
+
+    cnt++;
 }
 
 
@@ -42,29 +51,23 @@ int main()
 {
     cin >> t;
 
-    for (int i = 0; i < t; i++) {
+    while (t--) {
         cin >> m >> n >> k;
 
-        for (int j = 0; j < k; j++) {
+        Initialize();
+
+        while (k--) {
             cin >> x >> y;
             map[y][x] = 1;
         }
 
         cnt = 0;
-        for (int y = 0; y < n; y++) {
-            for (int x = 0; x < m; x++) {
-                if (map[y][x] == 1) {
-                    BFS(x, y);
-                    cnt++;
-                }
-            }
-        }
-
-        cout << cnt << '\n';
-
         for (int y = 0; y < n; y++)
             for (int x = 0; x < m; x++)
-                map[y][x] = 0;
+                if (map[y][x] == 1)
+                    Count(x, y);
+
+        cout << cnt << '\n';
     }
 
     return 0;
