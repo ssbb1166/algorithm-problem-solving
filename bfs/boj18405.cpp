41a91a031b2898e5c582ba40b85b1d8e45c037f5
Subin Song <mugong66@naver.com>
2024-07-20 15:25:57 +0900

    refactor: remove SpreadVirus function in boj18405.cpp
    Change a few things of functions:
      - rename BFS() to Spread()
      - remove SpreadVirus()
    
    Change next node range check code more intuitive:
      - 'nx <= 0' to 'nx < 1'
      - 'ny <= 0' to 'ny < 1'

--- bfs/boj18405.cpp
+++ bfs/boj18405.cpp
@@ -3,60 +3,34 @@
 using namespace std;
 
 
-int n, k, s, x, y, map[201][201], virus[201][201];
-queue<pair<int, int>> q, vq;
+int n, k, s, x, y, map[201][201], t[201][201];
+queue<pair<int, int>> q;
 
 
 int dx[] = { 0, 0, -1, 1 };
 int dy[] = { -1, 1, 0, 0 };
 
 
-int BFS(int t)
+void Spread()
 {
     int q_size = q.size();
-
-    while (q_size--) {
+    for (int i = 0; i < q_size; i++) {
         int x = q.front().first;
         int y = q.front().second;
-        vq.push({ x, y });
         q.pop();
 
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
 
-            if (nx <= 0 || nx > n || ny <= 0 || ny > n)
+            if (nx < 1 || nx > n || ny < 1 || ny > n)
                 continue;
 
-            if (map[nx][ny] == 0) {
+            if (map[nx][ny] == 0 || (map[nx][ny] > map[x][y] && t[nx][ny] == t[x][y] + 1)) {
                 q.push({ nx, ny });
-                map[nx][ny] = t + 1;
+                map[nx][ny] = map[x][y];
+                t[nx][ny] = t[x][y] + 1;
             }
-        }
-    }
-
-    return t + 1;
-}
-
-
-void SpreadVirus(int t)
-{
-    int q_size = vq.size();
-
-    while (q_size--) {
-        int x = vq.front().first;
-        int y = vq.front().second;
-        vq.pop();
-
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (nx <= 0 || nx > n || ny <= 0 || ny > n)
-                continue;
-
-            if (map[nx][ny] == t && virus[nx][ny] > virus[x][y] || !virus[nx][ny])
-                virus[nx][ny] = virus[x][y];
         }
     }
 }
@@ -68,20 +42,18 @@ int main()
 
     for (int x = 1; x <= n; x++) {
         for (int y = 1; y <= n; y++) {
-            cin >> virus[x][y];
-            if (virus[x][y]) {
-                map[x][y] = 1;
+            cin >> map[x][y];
+            if (map[x][y] > 0)
                 q.push({ x, y });
-            }
         }
     }
 
     cin >> s >> x >> y;
 
-    for (int t = 1; t <= s; t++)
-        SpreadVirus(BFS(t));
+    for (int t = 0; t < s; t++)
+        Spread();
 
-    cout << virus[x][y];
+    cout << map[x][y];
 
     return 0;
 }
