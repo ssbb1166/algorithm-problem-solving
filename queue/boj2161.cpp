957dd79fe71b0bc21c267e326b41d85f1c27c242
Subin Song <mugong66@naver.com>
2024-07-28 15:53:51 +0900

    feat: add queue/boj2161.cpp

---
+++ queue/boj2161.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+
+int n;
+queue<int> q;
+
+
+int main()
+{
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+        q.push(i);
+
+    while (!q.empty()) {
+        cout << q.front() << ' ';
+        q.pop();
+        q.push(q.front());
+        q.pop();
+    }
+
+    return 0;
+}
