c3215f85dadb215de18386a7a038c6ee009636dc
Subin Song <mugong66@naver.com>
2024-08-03 22:57:01 +0900

    feat: add mathematics/boj24313.cpp

---
+++ boj24313.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int a1, a0, c, n0;
+
+
+int main()
+{
+    cin >> a1 >> a0 >> c >> n0;
+
+    cout << (a1 <= c && a1 * n0 + a0 <= c * n0) ? 1 : 0;
+
+    return 0;
+}
