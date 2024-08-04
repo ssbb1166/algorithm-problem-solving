068409c2ed3a18eac287d5cdf9e2728794750449
Subin Song <mugong66@naver.com>
2024-08-04 11:34:34 +0900

    rename: boj24313.cpp to mathematics/boj24313.cpp

--- mathematics/boj24313.cpp
+++ mathematics/boj24313.cpp
@@ -8,8 +8,7 @@ int a1, a0, c, n0;
 int main()
 {
     cin >> a1 >> a0 >> c >> n0;
-
-    cout << (a1 <= c && a1 * n0 + a0 <= c * n0) ? 1 : 0;
+    cout << ((a1 <= c && a1 * n0 + a0 <= c * n0) ? 1 : 0);
 
     return 0;
 }
