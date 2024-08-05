909685e8dfaa3ac142178ccb485c11a0c5c15bfb
Subin Song <mugong66@naver.com>
2024-08-06 02:36:31 +0900

    feat: add mathematics/boj10972_b.cpp

---
+++ mathematics/boj10972_b.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+
+int n, seq[10000];
+
+
+int main()
+{
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+        cin >> seq[i];
+
+    if (next_permutation(seq, seq + n))
+        for (int i = 0; i < n; i++)
+            cout << seq[i] << ' ';
+    else
+        cout << -1;
+
+    return 0;
+}
