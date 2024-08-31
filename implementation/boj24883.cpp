878a7eadb86527a6451ba8629f7532a620c1aafe
Subin Song <mugong66@naver.com>
2024-08-31 20:33:51 +0900

    feat: add implementation/boj24883.cpp

---
+++ implementation/boj24883.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+char c;
+
+
+int main()
+{
+    cin >> c;
+
+    if (c == 'N' || c == 'n')
+        cout << "Naver D2";
+    else
+        cout << "Naver Whale";
+
+    return 0;
+}
