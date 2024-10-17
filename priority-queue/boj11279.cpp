cc993acada318afac3ae2c9af200a3b4eac0d921
Subin Song <mugong66@naver.com>
2024-10-18 08:17:50 +0900

    feat: add priority-queue/boj11279.cpp

---
+++ priority-queue/boj11279.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+
+int n, x;
+priority_queue<int> pq;
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n;
+
+    while (n--) {
+        cin >> x;
+
+        if (x > 0) {
+            pq.push(x);
+        }
+        else if (pq.empty()) {
+            cout << 0 << '\n';
+        }
+        else {
+            cout << pq.top() << '\n';
+            pq.pop();
+        }
+    }
+
+    return 0;
+}
