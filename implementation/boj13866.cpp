511c84ecd7099f3b60f11a2c4f510720a485b879
Subin Song <mugong66@naver.com>
2024-11-25 16:06:08 +0900

    feat: add implementation/boj13866.cpp

---
+++ implementation/boj13866.cpp
@@ -0,0 +1,15 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+
+int a, b, c, d;
+
+
+int main()
+{
+    cin >> a >> b >> c >> d;
+    cout << abs(a - b - c + d);
+
+    return 0;
+}
