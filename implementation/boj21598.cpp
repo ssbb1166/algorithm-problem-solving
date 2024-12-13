60c56562a004e2d130a88eebcee0f208fbedab69
Subin Song <mugong66@naver.com>
2024-12-13 22:46:23 +0900

    feat: add implementation/boj21598.cpp

---
+++ implementation/boj21598.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int n;
+
+
+int main()
+{
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+        cout << "SciComLove" << '\n';
+
+    return 0;
+}
