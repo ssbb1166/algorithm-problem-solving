9821527ac531676975dccd963614ab0bbc5c3f0b
Subin Song <mugong66@naver.com>
2025-02-16 04:05:20 +0900

    feat: add implementation/boj26574.cpp

---
+++ implementation/boj26574.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int n, num;
+
+
+int main()
+{
+    cin >> n;
+
+    while (n--) {
+        cin >> num;
+        cout << num << ' ' << num << '\n';
+    }
+
+    return 0;
+}
