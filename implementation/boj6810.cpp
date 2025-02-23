320d19ab1ab449e64778705b40ce431ce5b5176b
Subin Song <mugong66@naver.com>
2025-02-24 01:54:57 +0900

    feat: add implementation/boj6810.cpp

---
+++ implementation/boj6810.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int a, b, c;
+
+
+int main()
+{
+    cin >> a >> b >> c;
+    cout << "The 1-3-sum is " << 9 * 1 + 7 * 3 + 8 * 1 + 0 * 3 + 9 * 1 + 2 * 3 + 1 * 1 + 4 * 3 + 1 * 1 + 8 * 3 + a * 1 + b * 3 + c * 1;
+
+    return 0;
+}
