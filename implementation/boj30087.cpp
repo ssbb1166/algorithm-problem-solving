8ee2e82b531cfe7c8c130950c63d22927202805b
Subin Song <mugong66@naver.com>
2025-03-02 23:09:28 +0900

    feat: add implementation/boj30087.cpp

--- implementation/boj30087.cpp
+++ implementation/boj30087.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+
+int n;
+string seminar;
+
+
+int main()
+{
+    cin >> n;
+
+    while (n--) {
+        cin >> seminar;
+        if (seminar == "Algorithm")
+            cout << "204" << '\n';
+        if (seminar == "DataAnalysis")
+            cout << "207" << '\n';
+        if (seminar == "ArtificialIntelligence")
+            cout << "302" << '\n';
+        if (seminar == "CyberSecurity")
+            cout << "B101" << '\n';
+        if (seminar == "Network")
+            cout << "303" << '\n';
+        if (seminar == "Startup")
+            cout << "501" << '\n';
+        if (seminar == "TestStrategy")
+            cout << "105" << '\n';
+    }
+
+    return 0;
+}
