572a87ac22e1c45bf1b5b23a6ae36f68c110e9d8
Subin Song <mugong66@naver.com>
2025-03-08 18:16:20 +0900

    feat: add dp/boj11052.cpp

---
+++ dp/boj11052.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int a, b;
+
+
+int main()
+{
+    cin >> a >> b;
+
+    if (a < b)
+        cout << -1;
+    else if (a > b)
+        cout << 1;
+    else
+        cout << 0;
+
+    return 0;
+}
